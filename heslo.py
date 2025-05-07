8560f3697cc4dcc096f342c76451ac025e8fdee5
ZdenekHauer <zdenek.hauer@infis.cz>
2025-05-07 14:24:26 +0200

    Add files via upload

---
+++ heslo.py
@@ -0,0 +1,9 @@
+import random
+pole = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+vybrane = random.choice(pole)
+index_vybrane = pole.index(vybrane)
+print(f"Vybral jsem písmeno {vybrane} na indexu {index_vybrane}")
+
+
+
+

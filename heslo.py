21e58ef52d83735d6cf4eb4358feeafcaec8b160
unknown <zdenek.hauer@infis.cz>
2025-05-07 14:33:02 +0200

    done

--- heslo.py
+++ heslo.py
@@ -7,9 +7,3 @@ print(f"Vybral jsem písmeno {vybrane} na indexu {index_vybrane}")
 
 
 
-města = ["Praha","Brno","Ostrava","Plzeň","Olomouc"]
-v12 = random.choice(města)
-index = města.index(v12)
-
-print(f"Vybral jsem město {v12} co se vybralo na indexu {index} v poli")
-

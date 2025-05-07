84781b4df6b742007c8e841c771b3660c4c92f97
Martinsgame55 <martin.hoffmann@infis.cz>
2025-05-07 14:21:41 +0200

    Add files via upload

---
+++ heslo.py
@@ -0,0 +1,9 @@
+import random
+
+barvy = ["červená", "modrá", "zelená", "žlutá", "černá"]
+v9 = random.choice(barvy)
+index = barvy.index(v9)
+
+print(f"Vybral jsem barvu {v9} co se vybrala na indexu {index} v poli")
+
+
